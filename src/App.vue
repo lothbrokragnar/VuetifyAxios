ab90051e5d1041d06fe8d94934ab36c0c9f73f0d
lothbrokragnar <gonzalo.cuevas.verdugo@gmail.com>
2022-06-10 00:46:56 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+<template>
+  <v-app>
+
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
